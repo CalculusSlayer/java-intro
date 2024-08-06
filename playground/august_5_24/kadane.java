c493bb8f3546d2ada535ed9586073eb996b229c7
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-05 19:29:01 -0700

    initialized variables and introduced empty checking

--- playground/august_5_24/kadane.java
+++ playground/august_5_24/kadane.java
@@ -1,14 +1,25 @@
 package august_5_24;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 public class kadane {
     public static List<Integer> maxSum(List<Integer> nums) {
+        if (nums.isEmpty()) {
+            return new ArrayList<>(Arrays.asList(1));
+        }
+        int maxCurrent = nums.get(0);
+        int maxGlobal = nums.get(0);
+        int start = 0, end = 0, tempStart = 0;
 
+        for (int index = 1; index < nums.size()-1; index++) {
+            if 
+        }
         return Arrays.asList(0);
     }
     public static void main(String[] args) {
         System.out.println("8-5-24");
-        maxSum(Arrays.asList(1, 2, 3));
+        maxSum(Arrays.asList(-2, 1, -3, 4, -1, 2, 1, -5, 4));
     }
 }
