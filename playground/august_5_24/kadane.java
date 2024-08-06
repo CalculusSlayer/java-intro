d06427ac9cf2f39395e4bf7c642ef11c69ce0ce6
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-05 19:12:02 -0700

    set up kadane's algorithm file

---
+++ playground/august_5_24/kadane.java
@@ -0,0 +1,14 @@
+package august_5_24;
+
+import java.util.Arrays;
+import java.util.List;
+public class kadane {
+    public static List<Integer> maxSum(List<Integer> nums) {
+
+        return Arrays.asList(0);
+    }
+    public static void main(String[] args) {
+        System.out.println("8-5-24");
+        maxSum(Arrays.asList(1, 2, 3));
+    }
+}
