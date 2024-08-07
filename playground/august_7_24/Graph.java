847b383a9b2249b975f0600ac30502893c29c969
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-07 03:28:35 -0700

    defined adjacency list attribute

---
+++ playground/august_7_24/Graph.java
@@ -0,0 +1,11 @@
+package august_7_24;
+
+import java.util.*;
+
+public class Graph {
+    private HashMap<Integer, HashSet<Integer>> adjacencyList;
+
+    public Graph(int vertices) {
+        
+    }
+}
