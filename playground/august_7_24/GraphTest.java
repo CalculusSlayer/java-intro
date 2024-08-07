d36c28a2548bda826fcc9abd42826add47d547e9
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-07 03:55:10 -0700

    added graph test file

---
+++ playground/august_7_24/GraphTest.java
@@ -0,0 +1,16 @@
+package august_7_24;
+
+public class GraphTest {
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+
+        System.out.println(String.format("graph: %s", graph));
+    }
+}
