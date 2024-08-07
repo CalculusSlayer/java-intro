182711df64098190b4e63a135969695c892cbfb5
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-07 05:22:04 -0700

    added assertion statement

--- playground/august_7_24/GraphTest.java
+++ playground/august_7_24/GraphTest.java
@@ -5,12 +5,23 @@ public class GraphTest {
         Graph graph = new Graph(5);
         graph.addEdge(0, 1);
         graph.addEdge(0, 4);
+        graph.addEdge(0, 3);
         graph.addEdge(1, 2);
         graph.addEdge(1, 3);
         graph.addEdge(1, 4);
         graph.addEdge(2, 3);
         graph.addEdge(3, 4);
 
-        System.out.println(String.format("graph: %s", graph));
+        String expectedResult = """
+                                0 : 1 3 4
+                                1 : 2 3 4
+                                2 : 3
+                                3 : 4
+                                4 :
+                                """;
+        String actualResult = graph.toString();
+        assert actualResult.equals(expectedResult) : 
+        String.format("Expect `%s`\nbut instead got `%s`", expectedResult, actualResult);
+        System.out.println(String.format("graph:\n%s", graph));
     }
 }
